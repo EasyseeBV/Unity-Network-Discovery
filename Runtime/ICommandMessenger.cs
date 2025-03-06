18d028d4ff0b0da6dcba4aee49c7b69e192c4e24
EasyOeds <149141794+EasyOeds@users.noreply.github.com>
2025-03-06 14:10:29 +0100

    Local Network Discovery

--- Runtime/ICommandMessenger.cs
+++
@@ -1,9 +0,0 @@
-using Autron.Commands;
-using Unity.Netcode;
-using UnityEngine;
-
-public interface ICommandMessenger
-{
-    void SendCommandToAll(CommandData commandData);
-    void SendCommandToId(CommandData commandData, ulong pid);
-}
